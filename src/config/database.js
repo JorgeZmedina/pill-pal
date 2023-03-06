2965fef7fbcc1e741ec81d4891e35206710db7b3
capellandiana <capellandiana@yahoo.com>
2023-03-06 13:28:31 -0500

    adding things

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 //this is what connects to the mysql database
-const sequelize = new Sequelize('pillpalDB', 'username', 'password', {
+const sequelize = new Sequelize('pillpalDB', 'capellandiana', 'password', {
   host: 'localhost',
   dialect: 'mysql'
 });
