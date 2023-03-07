f1750c4e069cdb3a35c0fa264651887584a153c8
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-03-06 22:04:42 -0500

    renaming files

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-//this is what connects to the mysql database
-const sequelize = new Sequelize('pillpalDB', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-module.exports = sequelize;
